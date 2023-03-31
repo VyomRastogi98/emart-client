5eba404e6a4917249698c7b945f6806dc3628f88
VyomRastogi98 <vyomrastogi123@gmail.com>
2023-03-31 11:39:56 +0530

    working on ahome page

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sliderimage from '../Components/Sliderimage'
+import Header from '../Navigation/Header'
+import ProductX from '../Components/ProductX.js'
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Sliderimage/>
+      <ProductX/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Nav,Navbar,Container } from 'react-bootstrap'
+import LoginNav from '../Navigation/LoginNav'
+
+const Login = () => {
+  return (
+    <div>
+        <LoginNav/>
+    </div>
+
+  )
+}
+
+export default Login
